624580217b28a5809c616624d9b5fd76fc09d041
Carlos-Soto <carlossebastian.sotocastaneda.stu@ispascalcomandini.it>
2021-11-12 09:00:18 +0100

    ClasseB2

--- Biblioteca/Biblioteca/bibliotec.cs
+++ Biblioteca/Biblioteca/bibliotec.cs
@@ -8,6 +8,39 @@ namespace Biblioteca
 {
     class bibliotec
     {
+        string nomeB;
+        string indirizzo;
+        string apertura;
+        string chiusura;
+        List<libro> lista = new List<libro>;
 
+        public string NomeB
+        {
+            get;
+            set;
+        }
+
+        public string Indirizzo
+        {
+            get;
+            set;
+        }
+        public string Apertura
+        {
+            get;
+            set;
+        }
+        public string Chiusura
+        {
+            get;
+            set;
+        }
+        public bibliotec(string nomeB,string indirizzo,string apertura,string chiusura )
+        {
+            NomeB = nomeB;
+            Indirizzo = indirizzo;
+            Apertura = apertura;
+            Chiusura = chiusura;
+        }
     }
 }
