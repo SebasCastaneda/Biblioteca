2642d429f6a1a1754ac15cffc1c610c3d3a03ebc
Carlos-Soto <carlossebastian.sotocastaneda.stu@ispascalcomandini.it>
2021-11-12 09:28:34 +0100

    ClasseB3

--- Biblioteca/Biblioteca/bibliotec.cs
+++ Biblioteca/Biblioteca/bibliotec.cs
@@ -12,7 +12,7 @@ namespace Biblioteca
         string indirizzo;
         string apertura;
         string chiusura;
-        List<libro> lista = new List<libro>;
+        List<libro> lista = new List<libro>();
 
         public string NomeB
         {
@@ -35,12 +35,64 @@ namespace Biblioteca
             get;
             set;
         }
-        public bibliotec(string nomeB,string indirizzo,string apertura,string chiusura )
+        public bibliotec(string nomeB, string indirizzo, string apertura, string chiusura)
         {
             NomeB = nomeB;
             Indirizzo = indirizzo;
             Apertura = apertura;
             Chiusura = chiusura;
+        }
+
+        public void add(libro Lista)
+        {
+            lista.Add(Lista);
+        }
+        public string ricercaL(string nome)
+        {
+            string risposta = "si c'e il libro";
+            foreach (bibliotec lista in x)
+            {
+                if (lista.nomeB == nome)
+                {
+                  risposta="si c'e il libro";
+                }
+                else
+                {
+                    risposta = "non c'e";
+                    
+                }
+            }
+
+            return risposta;
+        }
+
+        public string RAutore(string Autore)
+        {
+            string libro="";
+            foreach (bibliotec lista in x)
+            {
+                if (lista.autore == Autore)
+                {
+                    libro = libro + lista.autore;
+                }
+               
+            }
+
+            return libro;
+        }
+
+        public int contare()
+        {
+           int n=0;
+            foreach (bibliotec lista in x)
+            {
+                if (lista.nomeB != "")
+                {
+                    n++;
+                }
+            }
+
+            return n;
         }
     }
 }
